32a02c7666f24e9dcab14146547187b2ebbfc425
Marc RDWZ <redwerkz@gmail.com>
2024-06-05 05:06:39 +0200

    👷  Migrate to vitest

---
+++ cli.test.js
@@ -0,0 +1,15 @@
+import { execa } from 'execa'
+import { describe, it, expect } from 'vitest'
+import packageJson from './package.json'
+
+describe('cli', () => {
+  it('displays the correct help message', async () => {
+    const { stdout } = await execa('./cli.js', ['--help'])
+    expect(stdout).toMatchSnapshot('help-message')
+  })
+
+  it('displays the correct version', async () => {
+    const { stdout } = await execa('./cli.js', ['--version'])
+    expect(stdout).toBe(packageJson.version)
+  })
+})
