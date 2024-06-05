fc9009b3229f82f095370e3444d98a6bbac79efc
Marc RDWZ <redwerkz@gmail.com>
2024-06-05 05:07:36 +0200

    ♻️  Update

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-'use strict';
-const compareSize = require('compare-size');
-const meow = require('meow');
-const prettyBytes = require('pretty-bytes');
+import compareSize from "compare-size"
+import meow from "meow"
+import prettyBytes from "pretty-bytes"
 
-const cli = meow(`
+const cli = meow(
+	`
 	Usage
 	  $ compare-size <file> <file>
 
@@ -13,17 +13,30 @@ const cli = meow(`
 	  foo.zip: 1.34 kB
 	  bar.tar.gz: 1 kB
 	  Difference: 0.34 kB
-`);
+`,
+	{
+		importMeta: import.meta,
+		flags: {},
+	},
+)
 
 if (cli.input.length < 2) {
-	console.error('Provide two files to compare');
-	process.exit(1);
+	console.error("Provide two files to compare ⚠️")
+	process.exit(1)
 }
 
-compareSize(cli.input[0], cli.input[1]).then(data => {
-	console.log([
-		`${cli.input[0]}: ${prettyBytes(data[cli.input[0]])}`,
-		`${cli.input[1]}: ${prettyBytes(data[cli.input[1]])}`,
-		`Difference: ${prettyBytes(data.difference)}`
-	].join('\n'));
-});
+compareSize(cli.input[0], cli.input[1]).then((data) => {
+	console.log(
+		[
+			`${cli.input[0].padEnd(Math.max(5, cli.input[0].length, cli.input[1].length))}: ${prettyBytes(
+				data[cli.input[0]],
+			)}`,
+			`${cli.input[1].padEnd(Math.max(5, cli.input[0].length, cli.input[1].length))}: ${prettyBytes(
+				data[cli.input[1]],
+			)}`,
+			`${"Delta".padEnd(Math.max(5, cli.input[0].length, cli.input[1].length))}: ${prettyBytes(
+				data.difference,
+			)}`,
+		].join("\n"),
+	)
+})
